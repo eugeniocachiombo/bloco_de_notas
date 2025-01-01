a1395059c0f7713db852ce764849fc7556004977
eugeniocachiombo <eugeniocachiombo@gmail.com>
2025-01-01 02:54:45 +0100

    Criando tabela apartir da classe conexao

--- src/conexao/Conexao.java
+++ src/conexao/Conexao.java
@@ -1,18 +1,46 @@
-
 package conexao;
 
+import classes.Nota;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class Conexao {
+
     public Connection getConexao() {
         try {
             String host = "jdbc:sqlite:src/conexao/conexao.db";
             Connection con = DriverManager.getConnection(host);
-            return con;
+            boolean tbl = this.criarTabela(con);
+             if(tbl){
+                System.out.println("Tabela criada");
+                return con;
+            }else{
+                System.out.println("Erro ao criar tabela");
+                return null;
+             }
+            
         } catch (Exception e) {
             System.out.println("Erro de conexão: " + e.getMessage());
             return null;
+        }
+    }
+
+    public boolean criarTabela(Connection con) {
+        try {
+            String sql = "create table if not exists nota (\n"
+                    + "    id Integer primary key AUTOINCREMENT,\n"
+                    + "    titulo varchar,\n"
+                    + "    descricao text,\n"
+                    + "    criacao varchar default CURRENT_TIMESTAMP,\n"
+                    + "    actualizacao varchar default CURRENT_TIMESTAMP\n"
+                    + ");";
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.execute();
+            return true;
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+            return false;
         }
     }
 }
